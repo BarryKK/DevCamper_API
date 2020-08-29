9fb25c3b36a40419970678d2852ecc0ce804794b
pyk <1748418812@qq.com>
2020-08-29 15:00:02 +0800

    login user

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -6,19 +6,23 @@ const {
   updateBootcamp,
   deleteBootcamp,
   getBootcampsInRadius,
+  bootcampPhotoUpload,
 } = require("../controllers/bootcamps");
-
+const advancedResults = require("../middleware/advancedResults");
+const Bootcamp = require("../models/Bootcamp");
 // Include other resource routers
 const courseRouter = require("./courses");
-
 const router = express.Router();
 
 // Re-route into other resource routers
 router.use("/:bootcampId/courses", courseRouter);
 
 router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
-
-router.route("/").get(getBootcamps).post(createBootcamp);
+router.route("/:id/photo").put(bootcampPhotoUpload);
+router
+  .route("/")
+  .get(advancedResults(Bootcamp, "courses"), getBootcamps)
+  .post(createBootcamp);
 
 router
   .route("/:id")
